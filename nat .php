28e50591fe4b4d85a77534b6e4ad08c1a97f96e4
natshaider <70336190+natshaider@users.noreply.github.com>
2021-07-18 11:47:15 -0700

    Add files via upload
    lab

---
+++ nat .php
@@ -0,0 +1,180 @@
+<fieldset style="font-size:18px; background-image: url('3.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%;">
+<h1 style="background-color:#00bdbd; height:110px; text-align:Center;"><br>Quiz Application</h1>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"style="padding-top: 10px">
+
+<b>1)     Who is making the Web standards?</b><br><br>
+<input type="radio" id="one" name="one" value="Mozilla">
+<label for="Mozilla">Mozilla</label>
+<input type="radio" id="one" name="one" value="Microsoft">
+<label for="Microsoft">Microsoft</label>
+<input type="radio" id="one" name="one" value="The World Wide Web Consortium">
+<label for="The World Wide Web Consortium">The World Wide Web Consortium</label>
+<input type="radio" id="one" name="one" value="NVDIA">
+<label for="NVDIA">NVDIA</label>
+
+<br><br><br>
+
+<b>2)   Which file controls how your frames will appear?</b><br><br>
+<input type="radio" id="two" name="two" value="Frameset">
+<label for="Frameset">Frameset</label>
+<input type="radio" id="two" name="two" value="Master Document">
+<label for="Master Document">Master Document</label>
+<input type="radio" id="two" name="two" value="Template">
+<label for="Template">Template</label>
+<input type="radio" id="two" name="two" value="Timeline">
+<label for="Timeline">Timeline</label>
+
+<br><br><br>
+
+<b>3)   What can't layers do if you want to convert them to tables?</b><br><br>
+<input type="radio" id="three" name="three" value="Be close">
+<label for="Be close">Be close</label>
+<input type="radio" id="three" name="three" value="Contain a Color">
+<label for="Contain a Color">Contain a Color</label>
+<input type="radio" id="three" name="three" value="Be larger than the target table">
+<label for="Be larger than the target table">Be larger than the target table</label>
+<input type="radio" id="three" name="three" value="Overlap">
+<label for="Overlap">Overlap</label>
+
+<br><br><br>
+
+<b>4)   Can a data cell contain images?</b><br><br>
+<input type="radio" id="four" name="four" value="Yes">
+<label for="Yes">Yes</label>
+<input type="radio" id="four" name="four" value=" No">
+<label for=" No"> No</label>
+
+
+<br><br><br>
+
+<b>5)   Which HTML attribute is used to define inline styles?</b><br><br>
+<input type="radio" id="five" name="five" value="font">
+<label for="font">font</label>
+<input type="radio" id="five" name="five" value="class">
+<label for="class">class</label>
+<input type="radio" id="five" name="five" value="styles">
+<label for="styles">styles</label>
+<input type="radio" id="five" name="five" value="style">
+<label for="style">style</label>
+
+<br><br><br>
+
+
+<b>6)   Which is the correct CSS syntax?</b><br><br>
+<input type="radio" id="six" name="six" value="body:color=black">
+<label for="body:color=black">body:color=black</label><br>
+<input type="radio" id="six" name="six" value="{body;color:black}">
+<label for="{body;color:black}">{body;color:black}</label><br>
+<input type="radio" id="six" name="six" value="{body:color=black(body}">
+<label for="{body:color=black(body}">{body:color=black(body}</label><br>
+<input type="radio" id="six" name="six" value="body {color: black}">
+<label for="body {color: black}">body {color: black}</label>
+
+<br><br><br>
+
+<b>7)   Using _______ statement is how you test for a specific condition.</b><br><br>
+<input type="radio" id="seven" name="seven" value="Select">
+<label for="Select">Select</label>
+<input type="radio" id="seven" name="seven" value="If">
+<label for="If">If</label>
+<input type="radio" id="seven" name="seven" value="Switch">
+<label for="Switch">Switch</label>
+<input type="radio" id="seven" name="seven" value="For">
+<label for="For">For</label>
+
+<br><br><br>
+
+<b>8)   Which of the following is the structure of an if statement?</b><br><br>
+<input type="radio" id="eight" name="eight" value="if (conditional expression is true) thenexecute this codeend if">
+<label for="if (conditional expression is true) thenexecute this codeend if">if (conditional expression is true) thenexecute this codeend if</label><br>
+<input type="radio" id="eight" name="eight" value="if (conditional expression is true)execute this codeend if">
+<label for="if (conditional expression is true)execute this codeend if">if (conditional expression is true)execute this codeend if</label><br>
+<input type="radio" id="eight" name="eight" value="if (conditional expression is true)   {then execute this code>->}">
+<label for="if (conditional expression is true)   {then execute this code>->}">if (conditional expression is true)   {then execute this code>->}</label><br>
+<input type="radio" id="eight" name="eight" value="if (conditional expression is true) then {execute this code}">
+<label for="if (conditional expression is true) then {execute this code}">if (conditional expression is true) then {execute this code}</label><br>
+
+<br><br><br>
+
+<b>9)   The _______ method of an Array object adds and/or removes elements from an array.</b><br><br>
+<input type="radio" id="nine" name="nine" value="Reverse">
+<label for="Reverse">Reverse</label>
+<input type="radio" id="nine" name="nine" value="Shift">
+<label for="Shift">Shift</label>
+<input type="radio" id="nine" name="nine" value="Slice">
+<label for="Slice">Slice</label>
+<input type="radio" id="nine" name="nine" value="Splice">
+<label for="Splice">Splice</label>
+
+<br><br><br>
+
+<b>10)   To set up the window to capture all Click events, we use which of the following statement?</b><br><br>
+<input type="radio" id="ten" name="ten" value=" window.captureEvents(Event.CLICK);">
+<label for=" window.captureEvents(Event.CLICK);"> window.captureEvents(Event.CLICK);</label><br>
+<input type="radio" id="ten" name="ten" value="window.handleEvents (Event.CLICK);">
+<label for="window.handleEvents (Event.CLICK);">window.handleEvents (Event.CLICK);</label><br>
+<input type="radio" id="ten" name="ten" value="window.routeEvents(Event.CLICK );">
+<label for="window.routeEvents(Event.CLICK );">window.routeEvents(Event.CLICK );</label><br>
+<input type="radio" id="ten" name="ten" value="window.raiseEvents(Event.CLICK );">
+<label for="window.raiseEvents(Event.CLICK );">window.raiseEvents(Event.CLICK );</label>
+
+<br><br><br>
+
+<input type='submit' id="$count" name="$count" value="Submit" style="height:50px; margin-left: 650px; width:200;" />
+</form>
+<hr style="border: 0.01px solid; color:Blue;">
+<p style=" font-size:20px;"><b>Result:
+<?php
+static $count = 0;
+$one = $two = $three = $four = $five = $six = $seven = $eight = $nine = $ten = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+if (isset($one)){
+$One = $_POST['one'];
+if($One == "Mozilla"){  $count++;  }}
+
+if (isset($two)){
+$Two = $_POST['two'];
+if($Two == "Frameset"){  $count++;  }}
+
+if (isset($three)){
+$Three = $_POST['three'];
+if($Three  == "Overlap"){  $count++;  }}
+
+if (isset($four)){
+$Four = $_POST['four'];
+if($Four == "Yes"){  $count++;  }}
+
+if (isset($five)){
+$Five = $_POST['five'];
+if($Five == "style"){  $count++;  }}
+
+if (isset($six)){
+$Six = $_POST['six'];
+if($Six == "body {color: black}"){  $count++;  }}
+
+if (isset($seven)){
+$Seven = $_POST['seven'];
+if($Seven == "If"){  $count++;  }}
+
+if (isset($eight)){
+$Eight = $_POST['eight'];
+if($Eight == "if (conditional expression is true)   {then execute this code>->}"){  $count++;  }}
+
+if (isset($nine)){
+$Nine = $_POST['nine'];
+if($Nine == "Splice"){  $count++;  }}
+
+if (isset($ten)){
+$Ten = $_POST['ten'];
+if($Ten == "window.captureEvents(Event.CLICK);"){  $count++;  }}
+
+echo($count);
+}
+?>
+<hr style="border: 0.01px solid; color:Blue;">
+</b>
+</p> 
+</fieldset>
